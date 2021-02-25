5a1579ac3b3bf6660ba1e1d928f16414e52df2d1
GronaldiNi <ja4761@student.uni-lj.si>
2021-02-25 11:58:38 +0100

    jezmes krezme

--- Gravitacija.java
+++ Gravitacija.java
@@ -4,5 +4,6 @@ public class Gravitacija{
     public static void main(String[] args){
         System.out.println("kurbadrek kurac taku ja");
         System.out.prinln("Rade je carry?");
+        System.out.prinln("jezus");
     }
 }
