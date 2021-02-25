0a9dcc869c0ccb4d7e79ce859839149d4cfb20bc
GronaldiNi <ja4761@student.uni-lj.si>
2021-02-25 11:54:46 +0100

    neko sporočilo kar č

--- Gravitacija.java
+++ Gravitacija.java
@@ -3,5 +3,6 @@ public class Gravitacija{
 
     public static void main(String[] args){
         System.out.println("kurbadrek kurac taku ja");
+        System.out.prinln("Rade je carry?");
     }
 }
