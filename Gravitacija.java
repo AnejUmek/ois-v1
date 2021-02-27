ff6c1b1cfe4e5b3129f6fa1ccaf3eaf2b039596d
RudolfMajster <gr1561@student.uni-lj.si>
2021-02-27 09:35:18 +0100

    Program za izracun gravitacijskega pospeska na dani nadmorski visini

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,6 +1,17 @@
+import java.util.*;
 public class Gravitacija{
     public static void main(String[] args){
-        System.out.println("Andrej je gej");
-        System.out.println("hrolin disi v sahu");
+        Scanner sc = new Scanner(System.in);
+        double v = sc.nextInt();
+        System.out.println(gravitacija(v));
+    }
+    public static double gravitacija(double v){
+        double C = 6.674 * Math.pow(10, -11);
+        double M = 5.972 * Math.pow(10, 24);
+        double r = 6.371 * Math.pow(10, 6);
+        double a = C * M;
+        double b = r + v;
+        double izracun = a / (Math.pow(b, 2));
+        return izracun;
     }
 }
